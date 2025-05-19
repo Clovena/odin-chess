a71762822f7e07082e97c96fbf77d5226aeef239
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-19 12:37:46 -0400

    Migrate work from knights travails into odin-chess repo.

---
+++ lib/knight.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+require_relative 'move'
+
+# Knight class
+class Knight
+  include Move
+
+  MOVEMENT = [
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1]
+  ].freeze
+
+  attr_accessor :loc, :children
+
+  def initialize(loc = [1, 1])
+    @loc = loc
+    @children = Board.available_squares(self)
+  end
+
+  def moves
+    MOVEMENT
+  end
+end
