69f10e77405e6adeb7524d5419a00746d20000e1
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-19 13:23:25 -0400

    Introduce bishop.

---
+++ lib/bishop.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+
+# Rook class
+class Bishop < Piece
+  def moves
+    moves_arr = []
+    range = [*-7..7]
+    range.each do |elem|
+      moves_arr << [elem, elem] << [-elem, elem] unless elem.zero?
+    end
+    moves_arr
+  end
+end
--- main.rb
+++ main.rb
@@ -1,21 +1,30 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require_relative 'lib/piece'
 require_relative 'lib/knight'
+require_relative 'lib/bishop'
 require_relative 'lib/rook'
 require_relative 'lib/board'
 require_relative 'lib/notation'
 
-def test_piece(piece)
-  puts "The knight is on #{piece.loc}."
+def test_piece(piece, name)
+  puts "The #{name} is on #{piece.loc}."
   puts "In chess notation, this is #{Notation.coords_to_algebraic(piece.loc, piece)}."
-  puts "The knight can move like: #{piece.moves}"
-  puts "The knight can move to: #{piece.children}"
+  puts "The #{name} can move like: #{piece.moves}"
+  puts "The #{name} can move to: "
+  move_list = ''
+  piece.children.each do |move|
+    move_list << "#{Notation.coords_to_algebraic(move)} "
+  end
+  puts move_list
   puts
 end
 
 test_knight = Knight.new([3, 3])
-test_piece(test_knight)
+test_piece(test_knight, 'knight')
 
 test_rook = Rook.new([4, 5])
-test_piece(test_rook)
+test_piece(test_rook, 'rook')
+
+test_bishop = Bishop.new([2, 4])
+test_piece(test_bishop, 'bishop')
