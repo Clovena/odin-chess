26d3f1fc7022400a91a6caace52fbf7338753b95
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-19 14:33:09 -0400

    Test pawn.

--- main.rb
+++ main.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: false
 
 require_relative 'lib/piece'
+require_relative 'lib/pawn'
 require_relative 'lib/knight'
 require_relative 'lib/bishop'
 require_relative 'lib/rook'
 require_relative 'lib/queen'
 require_relative 'lib/king'
-require_relative 'lib/board'
+require_relative 'lib/move'
 require_relative 'lib/notation'
 
-def test_piece(piece, name)
+def test_piece(piece)
+  name = "#{piece.player} #{piece.class}"
   puts "The #{name} is on #{piece.loc}."
   puts "In chess notation, this is #{Notation.coords_to_algebraic(piece.loc, piece)}."
   puts "The #{name} can move like: #{piece.moves}"
@@ -22,5 +24,5 @@ def test_piece(piece, name)
   puts
 end
 
-test_bishop = Bishop.new([5, 5])
-test_piece(test_bishop, 'bishop')
+test_pawn = Pawn.new(:white, [1, 2])
+test_piece(test_pawn)
