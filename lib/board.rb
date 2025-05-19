2afa3b7507d44934c213e764a0f460920edaba54
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-19 12:47:42 -0400

    Move notation-related methods to new module Notation.

--- lib/board.rb
+++ lib/board.rb
@@ -20,29 +20,4 @@ module Board
     end
     squares
   end
-
-  ### Methods to convert between coordinates and algebraic notation
-  def self.piece_letter(obj)
-    return '' if obj.nil?
-
-    piece = obj.class.to_s
-    case piece
-    when 'Knight' then 'N'
-    when 'Pawn' then ''
-    else piece[0]
-      # King, Queen, Rook, Bishop all convert to first letter, upcase
-    end
-  end
-
-  def self.coords_to_algebraic(coords, piece = nil)
-    piece_letter(piece) + FILES[coords[0]] + coords[1].to_s
-  end
-
-  def self.algebraic_to_coords(alg)
-    alg_arr = alg.split('')
-    [
-      FILES.index(alg_arr[0]),
-      alg_arr[1].to_i
-    ]
-  end
 end
---
+++ lib/notation.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Methods to convert between coordinates and algebraic notation
+module Notation
+  include Board
+
+  def self.piece_letter(piece_obj)
+    return '' if piece_obj.nil?
+
+    piece = piece_obj.class.to_s
+    case piece
+    when 'Knight' then 'N'
+    when 'Pawn' then ''
+    else piece[0]
+      # King, Queen, Rook, Bishop all convert to first letter, upcase
+    end
+  end
+
+  def self.coords_to_algebraic(coords, piece = nil)
+    piece_letter(piece) + FILES[coords[0]] + coords[1].to_s
+  end
+
+  def self.algebraic_to_coords(alg)
+    alg_arr = alg.split('')
+    [
+      FILES.index(alg_arr[0]),
+      alg_arr[1].to_i
+    ]
+  end
+end
