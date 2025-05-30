30f3bd8ba5bf20da95811d2da12378828ddbb3e9
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-05-29 23:11:00 -0400

    Require Move and Notation modules again.

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'chess/board'
-# require_relative 'chess/move'
-# require_relative 'chess/notation'
+require_relative 'chess/move'
+require_relative 'chess/notation'
 require_relative 'chess/piece'
 require_relative 'chess/pawn'
 require_relative 'chess/knight'
